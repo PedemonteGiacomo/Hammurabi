53a315bd4573100c8f1c6cbc5c3f43d9e0674954
Giacomo Pedemonte <giacomo.pedemonte@esaote.com>
2025-06-04 16:28:02 +0200

    refactor: enhance webpack configuration with Node polyfills and clean up ModuleScopePlugin references

--- Hammurabi/hammurabi-ui/craco.config.js
+++ Hammurabi/hammurabi-ui/craco.config.js
@@ -1,14 +1,25 @@
-// craco.config.js
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+
 module.exports = {
-    webpack: {
-      configure: (webpackConfig) => {
-        webpackConfig.resolve.fallback = {
-          ...webpackConfig.resolve.fallback,
-          fs: false, // disable 'fs' polyfill
-          path: require.resolve('path-browserify'),
-        };
-        return webpackConfig;
-      },
+  webpack: {
+    configure: (webpackConfig) => {
+      webpackConfig.resolve.fallback = {
+        ...webpackConfig.resolve.fallback,
+        fs: false,
+        path: require.resolve('path-browserify'),
+      };
+
+      webpackConfig.plugins = webpackConfig.plugins.filter(
+        (p) => p.constructor.name !== 'ModuleScopePlugin'
+      );
+      if (webpackConfig.resolve && Array.isArray(webpackConfig.resolve.plugins)) {
+        webpackConfig.resolve.plugins = webpackConfig.resolve.plugins.filter(
+          (p) => p.constructor && p.constructor.name !== 'ModuleScopePlugin'
+        );
+      }
+      webpackConfig.plugins.push(new NodePolyfillPlugin());
+
+      return webpackConfig;
     },
-  };
-  
+  },
+};
