7c11e072541bf7f0468a7c0779e69e5747c296b8
Giacomo Pedemonte <74009749+PedemonteGiacomo@users.noreply.github.com>
2025-06-04 10:22:05 +0200

    Fix test to use ReactEcsCdkStack

--- hammurabi-cdk/tests/unit/test_react_eks_cdk_stack.py
+++ hammurabi-cdk/tests/unit/test_react_eks_cdk_stack.py
@@ -1,13 +1,13 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from react_eks_cdk.react_eks_cdk_stack import ReactEksCdkStack
+from react_ecs_cdk.react_ecs_cdk_stack import ReactEcsCdkStack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in react_eks_cdk/react_eks_cdk_stack.py
 def test_sqs_queue_created():
     app = core.App()
-    stack = ReactEksCdkStack(app, "react-eks-cdk")
+    stack = ReactEcsCdkStack(app, "react-eks-cdk")
     template = assertions.Template.from_stack(stack)
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
